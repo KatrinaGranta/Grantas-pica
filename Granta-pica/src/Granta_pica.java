70fcc770b8c4d5fff80e47f1092bc41f1156a232
Katrina <katrinagranta30@gmail.com>
2021-02-24 11:30:48 +0200

    Pievienota switch funkcija

--- Granta-pica/src/Granta_pica.java
+++ Granta-pica/src/Granta_pica.java
@@ -8,7 +8,22 @@ public class Granta_pica {
 		do {
 			JOptionPane.showMessageDialog(null, "Ēdienu karte");
 			picasIzvele = JOptionPane.showInputDialog("1-Klienta info | 2-apskatīt klienta info | 3-pasūtīt| x-close");
-			picasIzvele = picasIzvele.toLowerCase();		
+			picasIzvele = picasIzvele.toLowerCase();	
+			switch(picasIzvele) {
+			case "1":
+			break;
+			case "2":
+			break;
+			case "3":
+				break;
+			case "x":
+				JOptionPane.showMessageDialog(null, "Programma apturēta!", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+				break;
+				
+			default: 
+		 		JOptionPane.showMessageDialog(null, "Tāda izvēle nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
+		 	break;
+			}
 		}while(!picasIzvele.equals("x"));
 
 	}
