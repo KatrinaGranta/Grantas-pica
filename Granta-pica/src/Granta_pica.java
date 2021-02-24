0655b46b49dab93df97093c050c62477487e7807
Katrina <katrinagranta30@gmail.com>
2021-02-24 11:28:47 +0200

    Veicu pirmās izmaiņas kodā, kur lietotājām tiek paprasīta izvēle

--- Granta-pica/src/Granta_pica.java
+++ Granta-pica/src/Granta_pica.java
@@ -1,8 +1,15 @@
+import javax.swing.JOptionPane;
 
 public class Granta_pica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String picasIzvele;
+		
+		do {
+			JOptionPane.showMessageDialog(null, "Ēdienu karte");
+			picasIzvele = JOptionPane.showInputDialog("1-Klienta info | 2-apskatīt klienta info | 3-pasūtīt| x-close");
+			picasIzvele = picasIzvele.toLowerCase();		
+		}while(!picasIzvele.equals("x"));
 
 	}
 
