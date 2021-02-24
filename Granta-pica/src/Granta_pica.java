4b964b4ffe997fed07d70336c64b7b670a30c79e
Katrina <katrinagranta30@gmail.com>
2021-02-24 11:33:29 +0200

    Izveidoju metodi, kas ļauj klientam ierakstīt informaciju par s sevi un saglabā to vienā failā

--- Granta-pica/src/Granta_pica.java
+++ Granta-pica/src/Granta_pica.java
@@ -1,6 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Granta_pica {
+	static String failaNosaukums1;
+	
+	public static void klientaInfo(){
+		try{
+			FileWriter ryt = new FileWriter("pasutijums");
+			BufferedWriter out=new BufferedWriter(ryt);
+			JTextField vards = new JTextField();
+			JTextField adrese = new JTextField();
+			JTextField telefons = new JTextField();
+			Object[] message = {    "Klienta vārds:", vards,"Klienta adrese:", adrese,"Klienta numurs:", telefons};
+			int answer = JOptionPane.showConfirmDialog(    null, message, "Ieraksti informāciju: ", JOptionPane.OK_CANCEL_OPTION);
+			if (answer == JOptionPane.OK_OPTION){    String klientaVards = vards.getText();
+			String KlientaAdrese = adrese.getText();
+			String TelNumurs = telefons.getText();
+			out.write("Klienta vārds: " +vards.getText()+"\n");
+			out.write("Klienta adrese: " +adrese.getText()+"\n");
+			out.write("Klienta numurs: " +telefons.getText()+"\n");
+			out.close();
+			JOptionPane.showMessageDialog(null, "Informācija ir ierakstīta!");
+			
+			}
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null, "Kļūme ierakstot failā!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 
 	public static void main(String[] args) {
 		String picasIzvele;
@@ -11,6 +40,7 @@ public class Granta_pica {
 			picasIzvele = picasIzvele.toLowerCase();	
 			switch(picasIzvele) {
 			case "1":
+				klientaInfo();
 			break;
 			case "2":
 			break;
